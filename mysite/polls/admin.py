2b3b8ccce4f8453776ded768fbc650372e2bbe80
wanglei10500 <651782999@qq.com>
2017-05-25 15:36:16 +0800

    polls

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Question,Choice
 # Register your models here.
+"""
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date','question_text']
+
+admin.site.register(Question,QuestionAdmin)
+"""
+class ChoiceInline(admin.TabularInline):  #TabularInline 基于表格的
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice)
